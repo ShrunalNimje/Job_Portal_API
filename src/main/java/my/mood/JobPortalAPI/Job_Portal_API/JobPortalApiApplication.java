643566c2b915309a74b58fdf297fcf63854c50b4
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-16 20:36:47 +0530

    Initial commit

---
+++ src/main/java/my/mood/JobPortalAPI/Job_Portal_API/JobPortalApiApplication.java
@@ -0,0 +1,13 @@
+package my.mood.JobPortalAPI.Job_Portal_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/mood/JobPortalAPI/Job_Portal_API/JobPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my.mood.JobPortalAPI.Job_Portal_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
