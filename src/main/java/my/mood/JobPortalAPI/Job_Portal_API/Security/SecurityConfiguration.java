0bdd898173de6c21775dfce7491a059709dd1203
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-23 00:54:51 +0530

    moving from hardcoded auth to logged in authentication from database

--- src/main/java/my/mood/JobPortalAPI/Job_Portal_API/Security/SecurityConfiguration.java
+++ src/main/java/my/mood/JobPortalAPI/Job_Portal_API/Security/SecurityConfiguration.java
@@ -2,22 +2,27 @@ package my.mood.JobPortalAPI.Job_Portal_API.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+		
+	CustomUserDetailsService customUserDetailsService;
+	
+	public SecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
+		this.customUserDetailsService = customUserDetailsService;
+	}
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,34 +44,17 @@ public class SecurityConfiguration {
 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 					)
 			
-			.httpBasic(httpBasic -> {});
+			.httpBasic(httpBasic -> {})
+		
+			.userDetailsService(customUserDetailsService);
 		
 		return http.build();
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-		
-		UserDetails admin = User.builder()
-				.username("Admin")
-				.password(passwordEncoder.encode("Admin123"))
-				.roles("ADMIN")
-				.build();
-		
-		UserDetails employer = User.builder()
-				.username("Employer")
-				.password(passwordEncoder.encode("Employer123"))
-				.roles("EMPLOYER")
-				.build();
-		
-		UserDetails jobSeeker = User.builder()
-				.username("JobSeeker")
-				.password(passwordEncoder.encode("JobSeeker123"))
-				.roles("JOB_SEEKER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(admin, employer, jobSeeker);
-	}
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
