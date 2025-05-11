c22eedda046b95f784d3cacf3855c5eadff14182
Shrunal Nimje <shrunalnimje@gmail.com>
2025-05-11 09:31:57 +0530

    changes in code

--- src/main/java/my/mood/JobPortalAPI/Job_Portal_API/Repository/JobRepository.java
+++ src/main/java/my/mood/JobPortalAPI/Job_Portal_API/Repository/JobRepository.java
@@ -1,5 +1,7 @@
 package my.mood.JobPortalAPI.Job_Portal_API.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,6 @@ import my.mood.JobPortalAPI.Job_Portal_API.Entity.Job_Entity;
 public interface JobRepository extends JpaRepository<Job_Entity, Integer>{
 	
 	public Page<Job_Entity> findAll(Pageable pageable);
+
+	public Optional<Job_Entity> findByTitleAndPostedBy(String title, String string);
 }
